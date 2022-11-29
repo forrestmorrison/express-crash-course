3168b9058ac5d85437fd4bdafc47c4675359f37d
forrestmorrison <forrestmorrison@gmail.com>
2022-11-29 15:53:08 -0600

    added logger to users.js

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-    console.log("Here")
     res.render("index", { text: "World" })
 })
 
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
-
-function logger(req, res, next)
 
 app.listen(3000)
